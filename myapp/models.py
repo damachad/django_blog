eeec983cc7679aed03fdb37dd4882ebc50172988
damachad <damachad@student.42porto.com>
2024-11-27 11:46:07 +0000

    Add User model

--- myapp/models.py
+++ myapp/models.py
@@ -1,5 +1,10 @@
 from django.db import models
 
-class Test(models.Model):
-	name = models.CharField(max_length=80)
+
+class User(models.Model):
+	name = models.CharField(max_length=100)
 	age = models.IntegerField(default=0)
+
+	def __str__(self):
+		return self.name
+	
