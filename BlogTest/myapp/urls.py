ab77c99abf7570747efb1e7d741b432f3457a6d2
damachad <damachad@student.42porto.com>
2025-01-03 13:09:15 +0000

    Add edit, add and delete post

--- BlogTest/myapp/urls.py
+++ BlogTest/myapp/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
-from .views import BlogPostListView, BlogPostDetailView
+from .views import BlogPostListView, BlogPostDetailView, BlogPostCreateView, BlogPostDeleteView, BlogPostUpdateView
 
 urlpatterns = [
-    path("posts/", BlogPostListView.as_view()),
-	path("posts/<int:pk>", BlogPostDetailView.as_view()),
+    path("", BlogPostListView.as_view(), name="postlist"),
+	path("posts/<int:pk>", BlogPostDetailView.as_view(), name="postdetail"),
+	path("posts/add", BlogPostCreateView.as_view(), name="addpost"),
+	path("posts/<int:pk>/edit", BlogPostUpdateView.as_view(), name="editpost"),
+	path("posts/<int:pk>/delete", BlogPostDeleteView.as_view(), name="deletepost"),
 ]
--- BlogTest/myapp/views.py
+++ BlogTest/myapp/views.py
@@ -1,5 +1,8 @@
 from django.views.generic import ListView, DetailView
+from django.views.generic.edit import CreateView, DeleteView, UpdateView
+from django.urls import reverse_lazy
 from .models import BlogPost
+import datetime
 
 
 # Create your views here.
@@ -9,4 +12,24 @@ class BlogPostListView(ListView):
 
 class BlogPostDetailView(DetailView):
     model = BlogPost
+
+
+class BlogPostCreateView(CreateView):
+    model = BlogPost
+    fields = ['title', 'subtitle', 'body']
+    def form_valid(self, form):
+
+        form.instance.date = datetime.date.today()
+        return super().form_valid(form)
+
+
+class BlogPostUpdateView(UpdateView):
+    model = BlogPost
+    fields = ['title', 'subtitle', 'body']
+    success_url = reverse_lazy("postlist")
+
+
+class BlogPostDeleteView(DeleteView):
+    model = BlogPost
+    success_url = reverse_lazy("postlist")
 
