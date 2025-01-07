142a2b171b46b3d233248d955724f421d933c689
damachad <davidmachado98@hotmail.com>
2025-01-07 10:15:01 +0000

    Add authentication logic

--- BlogTest/myapp/urls.py
+++ BlogTest/myapp/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import BlogPostListView, BlogPostDetailView, BlogPostCreateView, BlogPostDeleteView, BlogPostUpdateView
+from .views import *
 
 urlpatterns = [
-    path("", BlogPostListView.as_view(), name="postlist"),
+    path("", BlogPostListView.as_view(), name="home"),
+	path("sign-up", UserCreateView.as_view(), name="sign-up"),
 	path("posts/<int:pk>", BlogPostDetailView.as_view(), name="postdetail"),
 	path("posts/add", BlogPostCreateView.as_view(), name="addpost"),
 	path("posts/<int:pk>/edit", BlogPostUpdateView.as_view(), name="editpost"),
--- BlogTest/myapp/views.py
+++ BlogTest/myapp/views.py
@@ -1,7 +1,7 @@
 from django.views.generic import ListView, DetailView
 from django.views.generic.edit import CreateView, DeleteView, UpdateView
 from django.urls import reverse_lazy
-from .models import BlogPost
+from .models import BlogPost, User
 import datetime
 
 
@@ -23,13 +23,19 @@ class BlogPostCreateView(CreateView):
         return super().form_valid(form)
 
 
+class UserCreateView(CreateView):
+    model = User
+    fields = ['username', 'password']
+    success_url = reverse_lazy("login")
+
+
 class BlogPostUpdateView(UpdateView):
     model = BlogPost
     fields = ['title', 'subtitle', 'body']
-    success_url = reverse_lazy("postlist")
+    success_url = reverse_lazy("home")
 
 
 class BlogPostDeleteView(DeleteView):
     model = BlogPost
-    success_url = reverse_lazy("postlist")
+    success_url = reverse_lazy("home")
 
