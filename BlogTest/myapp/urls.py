f4c6cd4c2877c0779c338483a0366eca3115563e
damachad <davidmachado98@hotmail.com>
2025-01-07 11:22:22 +0000

    Require authentication for altering actions

--- BlogTest/myapp/urls.py
+++ BlogTest/myapp/urls.py
@@ -3,7 +3,7 @@ from .views import *
 
 urlpatterns = [
     path("", BlogPostListView.as_view(), name="home"),
-	path("sign-up", UserCreateView.as_view(), name="sign-up"),
+	path("register", UserCreateView.as_view(), name="register"),
 	path("posts/<int:pk>", BlogPostDetailView.as_view(), name="postdetail"),
 	path("posts/add", BlogPostCreateView.as_view(), name="addpost"),
 	path("posts/<int:pk>/edit", BlogPostUpdateView.as_view(), name="editpost"),
--- BlogTest/myapp/views.py
+++ BlogTest/myapp/views.py
@@ -2,7 +2,8 @@ from django.views.generic import ListView, DetailView
 from django.views.generic.edit import CreateView, DeleteView, UpdateView
 from django.urls import reverse_lazy
 from .models import BlogPost, User
-import datetime
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 
 # Create your views here.
@@ -14,28 +15,27 @@ class BlogPostDetailView(DetailView):
     model = BlogPost
 
 
-class BlogPostCreateView(CreateView):
+class BlogPostCreateView(LoginRequiredMixin, CreateView):
     model = BlogPost
     fields = ['title', 'subtitle', 'body']
-    def form_valid(self, form):
 
-        form.instance.date = datetime.date.today()
+    def form_valid(self, form):
+        form.instance.author = self.request.user
         return super().form_valid(form)
 
 
-class UserCreateView(CreateView):
-    model = User
-    fields = ['username', 'password']
-    success_url = reverse_lazy("login")
-
-
-class BlogPostUpdateView(UpdateView):
+class BlogPostUpdateView(LoginRequiredMixin, UpdateView):
     model = BlogPost
     fields = ['title', 'subtitle', 'body']
     success_url = reverse_lazy("home")
 
 
-class BlogPostDeleteView(DeleteView):
+class BlogPostDeleteView(LoginRequiredMixin, DeleteView):
     model = BlogPost
     success_url = reverse_lazy("home")
 
+
+class UserCreateView(CreateView):
+    form_class = UserCreationForm
+    template_name = 'registration/register.html'
+    success_url = reverse_lazy("login")
