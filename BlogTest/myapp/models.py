8b4e8df1731370823f748a9c4f3cc45b7631d699
damachad <davidmachado98@hotmail.com>
2025-01-07 11:48:34 +0000

    Clean comments

--- BlogTest/myapp/models.py
+++ BlogTest/myapp/models.py
@@ -1,25 +1,5 @@
 from django.db import models
 from django.contrib.auth.models import User
-# from django.contrib.auth.models import AbstractUser
-
-
-# Create your models here.
-# class User(AbstractUser):
-#     name = models.CharField(max_length=100, unique=True)
-
-#     # Override the groups field's related_name to avoid conflicts
-#     groups = models.ManyToManyField(
-#         'auth.Group',
-#         related_name='myapp_user_set',  # Add a unique related name to avoid clash
-#         blank=True
-#     )
-
-#     # Override the user_permissions field's related_name to avoid conflicts
-#     user_permissions = models.ManyToManyField(
-#         'auth.Permission',
-#         related_name='myapp_user_set',  # Add a unique related name to avoid clash
-#         blank=True
-#     )
 
 
 class BlogPost(models.Model):
