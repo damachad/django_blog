c8a11982a0ca449a8e1c1dbdf98688b6ec51e864
damachad <davidmachado98@hotmail.com>
2025-01-16 11:12:48 +0000

    Add boostrap

--- myapp/urls.py
+++ myapp/urls.py
@@ -1,4 +1,5 @@
 from django.urls import path, include, re_path
+from rest_framework.authtoken.views import obtain_auth_token
 from rest_framework.routers import DefaultRouter
 from .views import CustomUserViewSet, BlogPostViewSet, CommentViewSet, spa_index
 
@@ -9,5 +10,6 @@ router.register(r'comments', CommentViewSet)
 
 urlpatterns = [
     path('api/', include(router.urls)),
+	path('api-token-auth/', obtain_auth_token),
 	re_path(r'^.*$', spa_index, name='spa_index'),
 ]
