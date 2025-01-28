f17c28057f8131ae42c9152b5496a7bface6ad52
David Correia <davidmachado98@hotmail.com>
2025-01-28 18:40:50 +0000

    Add file to populate database

---
+++ myapp/management/commands/populate_db.py
@@ -0,0 +1,23 @@
+from django.core.management.base import BaseCommand
+from django.utils import lorem_ipsum
+from myapp.models import CustomUser, BlogPost
+
+class Command(BaseCommand):
+    help = 'Creates application data'
+
+    def handle(self, *args, **kwargs):
+        # get or create superuser
+        user = CustomUser.objects.filter(username='admin').first()
+        if not user:
+            user = CustomUser.objects.create_superuser(username='admin', password='test')
+
+        # create posts
+        posts = [
+            BlogPost(author=user, title="Beautiful Books", subtitle=lorem_ipsum.sentence(), body=lorem_ipsum.paragraph()),
+            BlogPost(author=user, title="A Random Post", subtitle=lorem_ipsum.sentence(), body=lorem_ipsum.paragraph()),
+            BlogPost(author=user, title="Top Secret", subtitle=lorem_ipsum.sentence(), body=lorem_ipsum.paragraph()),
+        ]
+
+        # create posts & re-fetch from DB
+        BlogPost.objects.bulk_create(posts)
+        posts = BlogPost.objects.all()
