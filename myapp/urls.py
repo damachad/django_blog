0ace8a75a0dd4a8ba86c22efb930fda5bdeff489
David Correia <davidmachado98@hotmail.com>
2025-01-29 17:25:53 +0000

    Update urls

--- myapp/urls.py
+++ myapp/urls.py
@@ -1,5 +1,4 @@
 from django.urls import path, include, re_path
-from rest_framework.authtoken.views import obtain_auth_token
 from rest_framework.routers import DefaultRouter
 from .views import CustomUserViewSet, BlogPostViewSet, CommentViewSet, spa_index
 
@@ -10,6 +9,6 @@ router.register(r'comments', CommentViewSet)
 
 urlpatterns = [
     path('api/', include(router.urls)),
-	path('api-token-auth/', obtain_auth_token),
+	path('', spa_index, name='home'),
 	re_path(r'^.*$', spa_index, name='spa_index'),
 ]
